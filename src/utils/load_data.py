8cf8d8d26cd252a3dc9881b429162f830cd6679c
Aldion Lee <lxdavid.lee@gmail.com>
2023-05-29 12:22:53 +0900

    Add function for loading data

---
+++ src/utils/load_data.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+import numpy as np
+from tensorflow import keras
+
+
+@dataclass
+class MnistData:
+    x_train: np.ndarray
+    x_test: np.ndarray
+    y_train: np.ndarray
+    y_test: np.ndarray
+
+    def preprocess(self):
+        self.X_train = self.x_train / 255
+        self.X_test = self.X_test / 255
+
+
+def load_data():
+    (x_train, y_train), (x_test, y_test) = keras.datasets.fashion_mnist.load_data()
+    return MnistData(x_train, x_test, y_train, y_test)
